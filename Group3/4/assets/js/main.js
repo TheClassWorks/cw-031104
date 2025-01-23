4e76a1833e36a9154bb01e6f098f2bd0ef3c9781
theclassworks <2300@mail.ir>
2025-01-23 17:41:50 +0330

    compidan

---
+++ Group3/4/assets/js/main.js
@@ -0,0 +1,12 @@
+// محاسبه میانگین
+
+
+let riazi = Number(prompt("نمره درس ریاضی را وارد کنید: "));
+let fizik = Number(prompt("نمره درس فیزیک را وارد کنید: "));
+let shimi = Number(prompt("نمره درس شیمی را وارد کنید: "));
+
+// پردازش
+let avg=(riazi+fizik+shimi)/3
+
+// output
+console.log(`moadele shoma ${avg} ast.`);
---
+++ Group3/5/assets/js/main.js
@@ -0,0 +1,9 @@
+// معادله درجه دو
+
+let a = Number(prompt("a را وارد کنید: "));
+let b = Number(prompt(" b را وارد کنید: "));
+let c = Number(prompt(" c را وارد کنید: "));
+
+let delta = b * b - 4 * a * c;
+
+console.log(`دلتای معادله شما ${delta} است`);
